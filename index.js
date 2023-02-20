6674fdf1eebf3949a1d0948f78453a787ec39322
inioluwa14 <63956918+inioluwa14@users.noreply.github.com>
2023-02-20 16:37:35 +0100

    solution

--- index.js
+++ index.js
@@ -31,5 +31,23 @@ else{
 }
 
 // Question 5
-let num = 4;
-let  
+function PowerOf2(num)
+{
+ 
+  // Calculate log2 of the num to get the smallest power of two to the number
+  var power = Math.floor(Math.log2(num));
+ 
+  // If 2^a is equal to N, return N
+  if (Math.pow(2, power) === num) {
+    return num;
+  }
+  else{
+    // Return 2^(a + 1)
+  return Math.pow(2, power + 1);
+  }
+}
+ 
+// Main function
+var number = 40;
+   
+console.log("The number " + PowerOf2(number) + " is the power of 2 nearest to " + number);
